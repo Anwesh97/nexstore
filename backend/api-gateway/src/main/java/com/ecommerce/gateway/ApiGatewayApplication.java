47992365cbc2a0b7fff06b5f21ee233dba5c5574
Anwesh Sahu <anwesh.sahu77@gmail.com>
2026-05-19 22:39:08 +0530

    added 2nd line anwesh1

--- backend/api-gateway/src/main/java/com/ecommerce/gateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/ecommerce/gateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(ApiGatewayApplication.class, args);
-    //hello        
+    //hello and hi asds    
+    //added 2nd line in anwesh1   
     }
 }
